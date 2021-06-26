1ce23781691e27509300a1fc00fdbe334d835f08
Kristofer Palmvik <kristofer@palmvik.se>
2021-06-26 13:32:06 +0200

    Extract Temperature component

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,13 @@
-import { useState, useEffect } from "react";
 import Content from "./Content";
 import Header from "./Header";
+import Temperature from "./Temperature";
 
 function App() {
-  const [hello, setHello] = useState();
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((response) => response.json())
-      .then((data) => setHello(data));
-  }, []);
   return (
     <>
       <Header />
       <Content>
-        <div>
-          Response from <code>/api/hello</code>:
-          <pre>{JSON.stringify(hello)}</pre>
-        </div>
+        <Temperature />
       </Content>
     </>
   );
