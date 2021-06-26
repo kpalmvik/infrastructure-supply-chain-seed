798972c7ba764fbcd3c1e99b744e2090c1b1059a
Kristofer Palmvik <kristofer@palmvik.se>
2021-06-26 16:48:01 +0200

    Default to type=button for Button

--- client/src/Button.jsx
+++ client/src/Button.jsx
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 import styles from "./Button.module.css";
 
-const Button = function Button({ children, secondary, type }) {
+const Button = function Button({ children, secondary, type = "button" }) {
   const buttonClassNames = classNames(styles.button, {
     [styles.secondary]: secondary,
   });
