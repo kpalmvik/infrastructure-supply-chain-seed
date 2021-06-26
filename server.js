87c68838e0f5fad1242f4cdbfc3a3444f85c6ec0
Kristofer Palmvik <kristofer@palmvik.se>
2021-06-26 12:40:44 +0200

    Remove redundant / response in server

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.get("/api/hello", (req, res) => {
   res.json({ hello: "world" });
 });
