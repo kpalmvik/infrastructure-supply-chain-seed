93a542321570c2b45228787d389db356548eeb59
Kristofer Palmvik <kristofer@palmvik.se>
2021-06-27 14:19:16 +0200

    Add Consumption component

---
+++ client/src/Consumption.jsx
@@ -0,0 +1,32 @@
+import useSWR from "swr";
+
+import Average from "./Average";
+import TemperatureBars from "./TemperatureBars";
+
+import styles from "./Consumption.module.css";
+
+const fetcher = (url) => fetch(url).then((r) => r.json());
+const swrOptions = { refreshInterval: 30 * 1000 };
+
+const Consumption = function Consumption({ children }) {
+  const { data, error } = useSWR("/api/consumption", fetcher, swrOptions);
+
+  return (
+    <div className={styles.consumption}>
+      {data && (
+        <>
+          <Average average={data.average} />
+          <TemperatureBars entries={data.items} />
+        </>
+      )}
+      {!data && (
+        <>
+          {error && <h2>Error loading consumption</h2>}
+          {!error && <h2>Loading consumption</h2>}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Consumption;
