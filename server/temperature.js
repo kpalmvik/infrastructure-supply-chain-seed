dc8e22e5f51befb63340d31a599232b22d9f643c
Kristofer Palmvik <kristofer@palmvik.se>
2021-06-27 08:47:19 +0200

    Fetch and format data from GraphQL endpoint

--- server/temperature.js
+++ server/temperature.js
@@ -1,18 +1,12 @@
 const fetch = require("node-fetch");
+const { GraphQLClient, gql } = require("graphql-request");
 
 const appOrigin = process.env.APP_ORIGIN;
 const email = process.env.EMAIL;
 const password = process.env.PASSWORD;
 
 const tokenURL = `${appOrigin}/v4/login.credentials`;
-
-const mockData = {
-  average: 10,
-  entries: [
-    18.4, 18.4, 17.8, 17.4, 17.9, 18.1, 17.6, 18, 17.5, 17.2, 18.2, 20.1, 20.2,
-    20.1, 19.9, 20.6, 21.5, 21.7, 21.7, 21.6, 21.1, 20.3, 19, 18,
-  ],
-};
+const apiEndpointURL = `${appOrigin}/v4/gql`;
 
 const getJWT = async function getJSONWebToken(email, password) {
   const response = await fetch(tokenURL, {
@@ -26,9 +20,46 @@ const getJWT = async function getJSONWebToken(email, password) {
   return await response.json();
 };
 
+const queryGraphQL = async function queryGraphQL(token) {
+  const query = gql`
+    {
+      me {
+        home(id: "a8c210fc-2988-4f06-9fe9-ab1bad9529d5") {
+          weather {
+            entries {
+              temperature
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const graphQLClient = new GraphQLClient(apiEndpointURL, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+
+  return await graphQLClient.request(query);
+};
+
+const meanValue = (entries) => entries.reduce((a, b) => a + b) / entries.length;
+
+const format = function formatData(data) {
+  const rawEntries = data.me.home.weather.entries;
+
+  const entries = rawEntries.map((entry) => entry.temperature);
+  const average = Math.round(meanValue(entries));
+
+  return { average, entries };
+};
+
 const temperature = async (req, res) => {
-  const token = await getJWT(email, password);
-  res.json({ ...mockData, token });
+  const jwt = await getJWT(email, password);
+  const data = await queryGraphQL(jwt.token);
+  const formattedData = format(data);
+  res.json(formattedData);
 };
 
 module.exports = temperature;
